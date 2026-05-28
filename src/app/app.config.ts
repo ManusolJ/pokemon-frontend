282d5a4877633a318784f56f98e1ccc3e02c5c51
Manu <manusolerj@gmail.com>
2026-05-28 22:14:20 +0200

    Chore: Updated app configuration to apply interceptors and message service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { jwtInterceptor } from '@core/interceptors/jwt.interceptor';
+import { errorInterceptor } from '@core/interceptors/error.interceptor';
 
 import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
+import { MessageService } from 'primeng/api';
 import { providePrimeNG } from 'primeng/config';
+
+import { provideRouter } from '@angular/router';
+
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
+    MessageService,
     provideRouter(routes),
+    provideHttpClient(withInterceptors([jwtInterceptor, errorInterceptor])),
+    provideBrowserGlobalErrorListeners(),
     providePrimeNG({
       theme: {
         preset: Aura,
