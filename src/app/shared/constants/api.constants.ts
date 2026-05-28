37282562336f0f3db85e858d22c24839c8f8e5d9
Manu <manusolerj@gmail.com>
2026-05-28 16:23:57 +0200

    Fix: refactored remaining endpoints for consistency

--- src/app/shared/constants/api.constants.ts
+++ src/app/shared/constants/api.constants.ts
@@ -10,6 +10,6 @@ export const EFFECTIVENESS_ENDPOINT = 'effectiveness';
 export const LOGIN_ENDPOINT = 'login';
 export const LOGOUT_ENDPOINT = 'logout';
 export const REFRESH_ENDPOINT = 'refresh';
-export const REGISTER_PARAMETER = 'register';
+export const REGISTER_ENDPOINT = 'register';
 export const PASSWORD_RESET_ENDPOINT = 'password-reset';
 export const PASSWORD_RESET_REQUEST_ENDPOINT = 'password-reset-request';
