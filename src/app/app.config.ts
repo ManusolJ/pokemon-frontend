d5a3e1c0bcc505194e848476d564d34e2432493c
Manu <manusolerj@gmail.com>
2026-05-29 02:11:11 +0200

    Feat: PrimeNG preset configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { jwtInterceptor } from '@core/interceptors/jwt.interceptor';
 import { errorInterceptor } from '@core/interceptors/error.interceptor';
 
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 
 import { routes } from './app.routes';
 
@@ -14,6 +15,24 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 
+const PokemonAura = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#FFFBEB',
+      100: '#FEF3C7',
+      200: '#FDE68A',
+      300: '#FCD34D',
+      400: '#FBBF24',
+      500: '#F59E0B',
+      600: '#D97706',
+      700: '#B45309',
+      800: '#92400E',
+      900: '#78350F',
+      950: '#451A03',
+    },
+  },
+});
+
 export const appConfig: ApplicationConfig = {
   providers: [
     MessageService,
@@ -22,7 +41,13 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: PokemonAura,
+        options: {
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng, utilities',
+          },
+        },
       },
     }),
   ],
