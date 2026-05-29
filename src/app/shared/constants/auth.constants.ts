9fd9b106d4fe341c30b035b7a14f8bb7cbf137f6
Manu <manusolerj@gmail.com>
2026-05-29 03:44:56 +0200

    Chore: Added more auth constants to file

--- src/app/shared/constants/auth.constants.ts
+++ src/app/shared/constants/auth.constants.ts
@@ -1,2 +1,7 @@
 export const ROLE_USER = 'ROLE_USER';
 export const ROLE_ADMIN = 'ROLE_ADMIN';
+
+export const MIN_USERNAME_LENGTH = 3;
+export const MAX_USERNAME_LENGTH = 20;
+
+export const MIN_PASSWORD_LENGTH = 8;
