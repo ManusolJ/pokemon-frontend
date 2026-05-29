e8677d771a7059b83dd0d2645e5f9999e16628d0
Manu <manusolerj@gmail.com>
2026-05-29 02:10:53 +0200

    Feat: Implemented main view

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,15 @@
+import { Toast } from 'primeng/toast';
+
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from '@shared/components/navbar/navbar';
+import { Footer } from '@shared/components/footer/footer';
 
 @Component({
+  imports: [Navbar, Footer, Toast, RouterOutlet],
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.html',
   styleUrl: './app.css',
+  templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('pokemon-team-builder');
