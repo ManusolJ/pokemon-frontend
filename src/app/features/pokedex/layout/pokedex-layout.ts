a5c25c8fb1fa0fa2b77811b09585d0f12064c4bf
Manu <manusolerj@gmail.com>
2026-06-01 00:00:39 +0200

    Refactor: Changed pokedex layout to adapt to DRY conventions

--- src/app/features/pokedex/layout/pokedex-layout.ts
+++ src/app/features/pokedex/layout/pokedex-layout.ts
@@ -1,17 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
+import { TabNav } from '@shared/components/tab-nav/tab-nav';
 import { NavItem } from '@shared/interfaces/ui/generic/nav-item.interface';
 
 @Component({
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, TabNav],
   selector: 'app-pokedex-layout',
   styleUrl: './pokedex-layout.css',
   templateUrl: './pokedex-layout.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokedexLayout {
-  protected readonly items: NavItem[] = [
+  protected readonly tabs: NavItem[] = [
     { label: 'Pokemon', path: 'pokemon' },
     { label: 'Moves', path: 'moves' },
     { label: 'Type Chart', path: 'type-chart' },
