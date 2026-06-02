17b78c50e1efb41e23c2e3ca6adb94e88b5a37ce
Manu <manusolerj@gmail.com>
2026-06-02 02:11:15 +0200

    Chore: Mapped out all admin routes

--- src/app/features/admin/routes/admin.routes.ts
+++ src/app/features/admin/routes/admin.routes.ts
@@ -5,6 +5,32 @@ export const ADMIN_ROUTES: Routes = [
     path: '',
     loadComponent: () =>
       import('@features/admin/layout/admin-layout').then((mod) => mod.AdminLayout),
-    children: [],
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'users',
+      },
+      {
+        path: 'users',
+        loadComponent: () =>
+          import('@features/admin/components/user-list/user-list').then((mod) => mod.UserList),
+      },
+      {
+        path: 'users/:id',
+        loadComponent: () =>
+          import('@features/admin/components/user-edit/user-edit').then((mod) => mod.UserEdit),
+      },
+      {
+        path: 'seed',
+        loadComponent: () =>
+          import('@features/admin/components/admin-seed/admin-seed').then((mod) => mod.AdminSeed),
+      },
+      {
+        path: 'logs',
+        loadComponent: () =>
+          import('@features/admin/components/admin-logs/admin-logs').then((mod) => mod.AdminLogs),
+      },
+    ],
   },
 ];
