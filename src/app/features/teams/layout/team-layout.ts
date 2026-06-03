740fda0e0d40b63c77906a5af2add214f184cdf6
Manu <manusolerj@gmail.com>
2026-06-03 03:46:42 +0200

    Feat: Implement team layout with navigation and styling

--- src/app/features/teams/layout/team-layout.ts
+++ src/app/features/teams/layout/team-layout.ts
@@ -1,10 +1,21 @@
+import { NavItem } from '@shared/interfaces/ui/generic/nav-item.interface';
+
+import { TabNav } from '@shared/components/tab-nav/tab-nav';
+
+import { RouterOutlet } from '@angular/router';
+
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  imports: [],
+  imports: [TabNav, RouterOutlet],
   selector: 'app-team-layout',
   styleUrl: './team-layout.css',
   templateUrl: './team-layout.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TeamLayout {}
+export class TeamLayout {
+  protected readonly items: NavItem[] = [
+    { label: 'Shared Teams', path: 'shared-teams' },
+    { label: 'My Teams', path: 'my-teams' },
+  ];
+}
