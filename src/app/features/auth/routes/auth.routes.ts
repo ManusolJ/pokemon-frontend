e67b31f648dcc8d1b025bb20d994f9914dde2cd0
Manu <manusolerj@gmail.com>
2026-08-20 15:50:17 +0200

    feat: add titles to authentication routes for improved navigation

--- src/app/features/auth/routes/auth.routes.ts
+++ src/app/features/auth/routes/auth.routes.ts
@@ -7,11 +7,13 @@ export const AUTH_ROUTES: Routes = [
     children: [
       {
         path: 'login',
+        title: 'Sign In',
         loadComponent: () =>
           import('@features/auth/components/login/login').then((mod) => mod.Login),
       },
       {
         path: 'registration',
+        title: 'Create Account',
         loadComponent: () =>
           import('@features/auth/components/registration/registration').then(
             (mod) => mod.Registration,
@@ -19,6 +21,7 @@ export const AUTH_ROUTES: Routes = [
       },
       {
         path: 'reset-password-request',
+        title: 'Reset Password',
         loadComponent: () =>
           import('@features/auth/components/reset-password-request/reset-password-request').then(
             (mod) => mod.ResetPasswordRequest,
@@ -26,6 +29,7 @@ export const AUTH_ROUTES: Routes = [
       },
       {
         path: 'reset-password-confirmation',
+        title: 'Reset Password',
         loadComponent: () =>
           import('@features/auth/components/reset-password-confirmation/reset-password-confirmation').then(
             (mod) => mod.ResetPasswordConfirmation,
